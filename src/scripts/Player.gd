b2683c74e4cddb2589ae086fde84ee0ad5e33d57
CosmicRayBitFlip <43388851+CosmicRayBitFlip@users.noreply.github.com>
2024-01-05 22:05:31 -0600

    Implemented animation system, reworked code
    Eventually gonna change it so that the player faces your cursor

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -1,14 +1,14 @@
 extends KinematicBody2D
 
 const speed = 100
-# direction and animation constats
-const PA_NULL = 0
-const PA_X = -1
-const PA_Y = 1
-const UP = -1; const RIGHT = -1
-const DOWN = 1; const LEFT = 1
+# direction constants
+const UP = Vector2(0,-1)
+const RIGHT = Vector2(-1, 0)
+const DOWN = Vector2(0, 1)
+const LEFT = Vector2(1, 0)
+const NULL_DIR = Vector2.ZERO
 
-var input_vector:Vector3 = Vector3.ZERO # z component uses one of the primary axis constants above
+var input_vector:Vector2 = Vector2.ZERO
 var velocity:Vector2 = Vector2.ZERO
 
 func _ready():
@@ -18,14 +18,9 @@ func _input(event):
 	if event is InputEventKey:
 		input_vector.x = Input.get_axis("left", "right")
 		input_vector.y = Input.get_axis("up", "down")
-		if not input_vector.z:
-			input_vector.z = PA_X * abs(input_vector.x) # primary axis is x if left or right is pressed
-			input_vector.z += PA_Y * abs(input_vector.y) # primary axis is y if up or down is pressed
-		if not Vector2(input_vector.x, input_vector.y):
-			input_vector.z = PA_NULL
 
 func _process(delta):
-	update_animation(input_vector.x, input_vector.y, input_vector.z)
+	update_animation(input_vector)
 	velocity = process_movement(Vector2(input_vector.x, input_vector.y), delta)
 	move_and_collide(velocity)
 
@@ -35,9 +30,26 @@ func process_movement(input_vec, delta):
 	else:
 		return Vector2.ZERO
 
-func update_animation(x_dir, y_dir, primary_axis): # finish when animation is completed
-	if primary_axis == PA_X or primary_axis == PA_Y:
-		if true:
-			$AnimationPlayer.play("Walk")
+func update_animation(input_vec): # doesn't really matter if it's buggy right now, it's gonna be changed
+	if input_vec:
+		match input_vec:
+			LEFT: 
+				$AnimationPlayer.play("SideWalk") 
+				$Sprite.flip_h = true
+			RIGHT: 
+				$AnimationPlayer.play("SideWalk")
+				$Sprite.flip_h = false
+			UP:
+				$AnimationPlayer.play("BackWalk")
+				$Sprite.flip_h = false
+			DOWN:
+				$AnimationPlayer.play("FrontWalk")
+				$Sprite.flip_h = false
 	else:
-		$AnimationPlayer.play("Idle")
+		match $AnimationPlayer.assigned_animation:
+			"SideWalk":
+				$AnimationPlayer.play("SideIdle")
+			"FrontWalk":
+				$AnimationPlayer.play("FrontIdle")
+			"BackWalk":
+				$AnimationPlayer.play("BackIdle")
