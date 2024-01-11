d25cf5c52009bad22e89e11016711a4465847b75
CosmicRayBitFlip <43388851+CosmicRayBitFlip@users.noreply.github.com>
2024-01-10 19:41:54 -0600

    i have plans

--- src/scripts/classdefs/NPC.gd
+++ src/scripts/classdefs/NPC.gd
@@ -8,3 +8,5 @@ var dir:Vector2
 
 var team:int # team only has to be assigned for normal human robot (wait 'human robot'?)
 enum {WHITE, RED, BLUE, YELLOW} # teams
+
+# i don't even remember the reason i made this class if i was just gonna leave it blank
