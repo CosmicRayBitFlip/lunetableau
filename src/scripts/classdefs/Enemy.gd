4ee9604995b81b140600d9fa00c2791fdf0775b1
CosmicRayBitFlip <43388851+CosmicRayBitFlip@users.noreply.github.com>
2024-01-11 21:00:44 -0600

    Removed the team property and changed outdated name

--- src/scripts/classdefs/Enemy.gd
+++ src/scripts/classdefs/Enemy.gd
@@ -6,7 +6,7 @@ var hp:int
 var speed:int
 var dir:Vector2
 
-var team:int # team only has to be assigned for normal human robot (wait 'human robot'?)
 enum {WHITE, RED, BLUE, YELLOW} # teams
 
 # i don't even remember the reason i made this class if i was just gonna leave it blank
+# at least the team name constants are useful
