f88183eb4404547fd8668b9794dbffbf7655ada8
CosmicRayBitFlip <43388851+CosmicRayBitFlip@users.noreply.github.com>
2024-01-11 21:58:25 -0600

    Backup commit Jan. 11 2024

--- src/scenes/RobotEnemy.gd
+++ src/scenes/RobotEnemy.gd
@@ -1,7 +1,6 @@
 extends Enemy
 
-func spawn(robot_team:int, spawn_pos:Vector2, direction:Vector2 = Vector2.DOWN):
-	team = robot_team
+func spawn(spawn_pos:Vector2, direction:Vector2 = Vector2.DOWN):
 	position = spawn_pos
 	dir = direction
 	hp = 10
